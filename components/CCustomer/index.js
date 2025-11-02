38a1c9e740d5d25f8c2a2fc52dad274b856731a2
ly6158 <ly6158@qq.com>
2025-11-02 21:44:15 +0800

    增加客户展示组件

---
+++ components/CCustomer/index.js
@@ -0,0 +1,18 @@
+Component({
+  options: {
+    virtualHost: true,
+    addGlobalClass: true
+  },
+  properties: {
+    customer: {
+      type: Object,
+      value: ()=>({})
+    }
+  },
+  data: {
+
+  },
+  methods: {
+
+  }
+})
